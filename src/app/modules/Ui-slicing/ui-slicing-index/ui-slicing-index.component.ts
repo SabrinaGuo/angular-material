1ac826a41ce395427126ed2e588dbc1909f88052
SabrinaGuo <hsif0111@gmail.com>
2021-12-16 10:47:51 +0800

    add

--- src/app/modules/Ui-slicing/ui-slicing-index/ui-slicing-index.component.ts
+++ src/app/modules/Ui-slicing/ui-slicing-index/ui-slicing-index.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui-slicing-index',
   templateUrl: './ui-slicing-index.component.html',
-  styleUrls: ['./ui-slicing-index.component.scss']
+  styleUrls: ['./ui-slicing-index.component.scss'],
 })
 export class UiSlicingIndexComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
